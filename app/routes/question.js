4046505c3d838a8cf02b83698c91c41e5e2aaba3
njogu <munyiri_james@outlook.com>
2017-07-20 05:18:52 +0300

    node modules

--- app/routes/question.js
+++ app/routes/question.js
@@ -28,7 +28,7 @@ export default Ember.Route.extend({
       newAnswer.save().then(function() {
         return question.save();
       });
-      this.transitionTo('index');
+      this.transitionTo('question',question);
     }
   }
 });
